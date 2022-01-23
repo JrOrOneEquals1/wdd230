e4132480cbd78aa2c12e961028fc9dcd2621d6ed
JrOrOneEquals1 <cdr5of8@gmail.com>
2022-01-22 19:39:55 -0600

    idk

--- js/js.js
+++ js/js.js
@@ -1,6 +1,12 @@
 date = new Date();
 
 window.onload = function () {
+    var html = document.getElementsByTagName("html")[0];
+    var footer = document.createElement("footer");
+    html.appendChild(footer);
+    var text = document.createElement("p")
+    text.innerHTML = "(c) <span id='year'></span> | Cameron Roberts | Idaho | BYUI Online Learning | Last Updated: <span id='modified'>"
+    footer.appendChild(text)
     document.getElementById('year').innerText = date.getFullYear();
     document.getElementById('modified').innerText = document.lastModified;
 }
