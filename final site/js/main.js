73b364767f286a74f5ba1b252eaf6ee03f6ed5de
JrOrOneEquals1 <cdr5of8@gmail.com>
2022-04-06 14:39:21 -0600

    save again

--- final site/js/main.js
+++ final site/js/main.js
@@ -10,13 +10,12 @@ async function setLatLon(position) {
     var lon = Math.round(position.coords.longitude * 100) / 100;
     var response = await fetch("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=223c2347c067d492257c231fb1178c51");
     var json = await response.json();
+    console.log(json)
     try {
         document.getElementById('weather').innerHTML += `<p>The temperature in your location is currently: ${json['current']['temp']} &#176;F</p>`;
         document.getElementById('weather').innerHTML += `<p>The weather in your location is currently: ${json['current']['weather'][0]['main']}</p>`;
-        document.getElementById('forecast').innerHTML += `<table id="forecastTable"><tr><td>Today</td><td>${weekdays[date.getDay() + 1]}</td><td>${weekdays[date.getDay() + 2]}</td></tr><tr>`;
-        document.getElementById('forecast').innerHTML += `<td></td>`;
-        document.getElementById('forecast').innerHTML += `<td></td>`;
-        document.getElementById('forecast').innerHTML += `<td></td></tr></table>`;
+        document.getElementById('weather').innerHTML += `<p>The humidity in your location is currently: ${json['current']['humidity']}%</p>`;
+        document.getElementById('forecast').innerHTML += `<table id="forecastTable"><tr><td>Tomorrow</td><td>${weekdays[date.getDay() + 2]}</td><td>${weekdays[date.getDay() + 3]}</td></tr><tr><td><p>${json['daily'][0]['temp']['day']} &#176;F</p><p>${json['daily'][0]['weather'][0]['main']}</p></td><td><p>${json['daily'][1]['temp']['day']} &#176;F</p><p>${json['daily'][1]['weather'][0]['main']}</p></td><td><p>${json['daily'][2]['temp']['day']} &#176;F</p><p>${json['daily'][2]['weather'][0]['main']}</p></td></tr></table>`;
     }
     catch {}
     try {
@@ -30,4 +29,11 @@ async function setLatLon(position) {
         });
     }
     catch {}
+}
+
+date = new Date();
+
+function redirect() {
+    document.getElementById('datetime').value = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    window.location.href = "./thankyou.html";
 }
