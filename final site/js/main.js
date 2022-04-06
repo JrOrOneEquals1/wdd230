a5f823709a96f5e3f47725659d772813f221a16e
JrOrOneEquals1 <cdr5of8@gmail.com>
2022-04-06 17:09:26 -0600

    idk

--- final site/js/main.js
+++ final site/js/main.js
@@ -2,7 +2,13 @@ const date = new Date();
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 window.onload = function () {
-    navigator.geolocation.getCurrentPosition(setLatLon);
+    // try {
+    //     navigator.geolocation.getCurrentPosition(setLatLon);
+    // }
+    // catch {
+        var position = {coords: {latitude: 51.5, longitude: 0.1}}
+        setLatLon(position);
+    // }
 }
 
 async function setLatLon(position) {
@@ -10,12 +16,11 @@ async function setLatLon(position) {
     var lon = Math.round(position.coords.longitude * 100) / 100;
     var response = await fetch("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=223c2347c067d492257c231fb1178c51");
     var json = await response.json();
-    console.log(json)
     try {
-        document.getElementById('weather').innerHTML += `<p>The temperature in your location is currently: ${json['current']['temp']} &#176;F</p>`;
+        document.getElementById('weather').innerHTML += `<p>The temperature in your location is currently: ${Math.round(json['current']['temp'])} &#176;F</p>`;
         document.getElementById('weather').innerHTML += `<p>The weather in your location is currently: ${json['current']['weather'][0]['main']}</p>`;
         document.getElementById('weather').innerHTML += `<p>The humidity in your location is currently: ${json['current']['humidity']}%</p>`;
-        document.getElementById('forecast').innerHTML += `<table id="forecastTable"><tr><td>Tomorrow</td><td>${weekdays[date.getDay() + 2]}</td><td>${weekdays[date.getDay() + 3]}</td></tr><tr><td><p>${json['daily'][0]['temp']['day']} &#176;F</p><p>${json['daily'][0]['weather'][0]['main']}</p></td><td><p>${json['daily'][1]['temp']['day']} &#176;F</p><p>${json['daily'][1]['weather'][0]['main']}</p></td><td><p>${json['daily'][2]['temp']['day']} &#176;F</p><p>${json['daily'][2]['weather'][0]['main']}</p></td></tr></table>`;
+        document.getElementById('forecast').innerHTML += `<table id="forecastTable"><tr><td id='tableHeader'>Tomorrow</td><td id='tableHeader'>${weekdays[date.getDay() + 2]}</td><td id='tableHeader'>${weekdays[date.getDay() + 3]}</td></tr><tr><td id='info'><p>${Math.round(json['daily'][0]['temp']['day'])} &#176;F</p><p>${json['daily'][0]['weather'][0]['main']}</p></td><td id='info'><p>${Math.round(json['daily'][1]['temp']['day'])} &#176;F</p><p>${json['daily'][1]['weather'][0]['main']}</p></td><td id='info'><p>${Math.round(json['daily'][2]['temp']['day'])} &#176;F</p><p>${json['daily'][2]['weather'][0]['main']}</p></td></tr></table>`;
     }
     catch {}
     try {
@@ -23,17 +28,46 @@ async function setLatLon(position) {
         var temples = await templeInfo.json();
         temples.forEach(function (temple) {
             var ele = document.getElementById(temple["id"]);
-            ele.innerHTML = `<h2>The ${temple["temple"]} Temple</h2>`;
+            ele.innerHTML = `<h2>The ${temple["temple"]} Temple <img class='likeButton' id='${temple["id"]}LikeButton' src='images/thumb.png' width=15px></h2>`;
             ele.innerHTML += `<p>The ${temple["temple"]} temple was announced on ${temple["announced"]}.</p>`;
             ele.innerHTML += `<p>Construction started on ${temple["groundbreak"]}, and the temple was dedicated on ${temple["dedicated"]}.</p>`;
+            ele.innerHTML += `<p>They are located at ${temple["address"]}, and are open ${temple["ordinance"]} for ordinances, and ${temple["session"]} for sessions.</p>`;
+            ele.innerHTML += `<p>You can contact them by phone at ${temple['phone']}. They ${temple['services']}.</p>`;
+            ele.innerHTML += `<p>The next scheduled closure is on ${temple['closure']}.</p>`;
+            ele.innerHTML += `<p>Log in <a href="${temple['email']}">here</a> to send an email to this temple.</p>`;
         });
+        setClickEvents();
     }
     catch {}
 }
 
-date = new Date();
-
 function redirect() {
-    document.getElementById('datetime').value = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     window.location.href = "./thankyou.html";
+}
+
+
+function setClickEvents() {
+    $(".likeButton").click(function() {
+        $(this).toggleClass('liked');
+        var btnStorage = $(this).attr("id");
+        if(this.getAttribute('class').includes('liked')) {
+            localStorage.setItem(btnStorage, 'true');
+            this.setAttribute('src', 'images/thumb2.png');
+        }
+        else {
+            localStorage.removeItem(btnStorage, 'true');
+            this.setAttribute('src', 'images/thumb.png');
+        }
+    });
+
+    $(".likeButton").each(function() {
+        var mainlocalStorage = $(this).attr("id");
+        console.log(localStorage.getItem(mainlocalStorage))
+        if(localStorage.getItem(mainlocalStorage) == 'true') {
+            $(this).addClass("liked");
+            this.setAttribute('src', 'images/thumb2.png');
+        } else {
+            $(this).removeClass("liked");
+        }
+    });
 }
